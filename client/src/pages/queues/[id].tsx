4a597c4b17ab77ab429771bc9ed0230077ae0ba3
Diar Begisbayev <diar_beg64@mail.ru>
2023-05-13 15:25:59 +0600

    Queues page removed

--- client/src/pages/queues/[id].tsx
+++
@@ -1,7 +0,0 @@
-import { QueuesExcerptScreen } from "@/screens/queue-excerpt/QueueExcerptScreen";
-
-const QueuesExcerptPage = () => {
-  return <QueuesExcerptScreen />;
-};
-
-export default QueuesExcerptPage;
